c96f4920b561939c25ab2ba0d849869d83890564
Dun-Njuguna <duncankariuki60@gmail.com>
2019-06-28 14:51:54 +0300

    created signup route

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,20 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 const express = require('express');
-const app = express();   
+const app = express(); 
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDIkVg9yIYTGT_75sOCzmW4Z1RxOj4xQJ8",
+    authDomain: "react1-4ad5b.firebaseapp.com",
+    databaseURL: "https://react1-4ad5b.firebaseio.com",
+    projectId: "react1-4ad5b",
+    storageBucket: "react1-4ad5b.appspot.com",
+    messagingSenderId: "374018603976",
+    appId: "1:374018603976:web:60d5d56ad54bb8f8"
+  };
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
 
 app.get('/screams', (req,res) => {
     admin
@@ -45,5 +58,24 @@ app.post('/scream', (req, res) => {
             console.error(err);
         });
 });
+
+// Signup
+
+app.post('/signup', (req,res) => {
+    const newUser={
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+    }
+
+   firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+        return res.status(201).json({message: `user ${data.user.uid} signup successful`})
+    }) 
+    .catch( err => {
+        return res.status(500).json({error: err.code});
+    });
+})
 
 exports.api = functions.https.onRequest(app);
