1af65f4b1f521d111e81e1322625dd1bb14a261a
Dun-Njuguna <duncankariuki60@gmail.com>
2019-06-28 17:56:20 +0300

    created login route and middle weare route to add authorization when posting to database

--- functions/index.js
+++ functions/index.js
@@ -11,7 +11,7 @@ const app = express();
 var firebaseConfig = {
     apiKey: "AIzaSyDIkVg9yIYTGT_75sOCzmW4Z1RxOj4xQJ8",
     authDomain: "react1-4ad5b.firebaseapp.com",
-    databaseURL: "https://react1-4ad5b.firebaseio.com",
+     databaseURL: "https://react1-4ad5b.firebaseio.com",
     projectId: "react1-4ad5b",
     storageBucket: "react1-4ad5b.appspot.com",
     messagingSenderId: "374018603976",
@@ -41,15 +41,47 @@ app.get('/screams', (req,res) => {
     .catch(err => console.error(err));
 })
 
-app.post('/scream', (req, res) => {
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    }else{
+        console.error('No token found');
+        return Response.status(403).json({error: 'Unauthorized'});
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch(err => {
+            console.error('Error while verifying token' , err);
+            return res.status(403).json(err);
+        })
+}
+
+//post one scream
+app.post('/scream', FBAuth, (req, res) => {
+
+    if(req.body.body.trim() === ''){
+        return res.status(400).json({body: 'Body must not be empty'});
+    }
+
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     };
 
-    db
-        .collection('screams')
+    db.collection('screams')
         .add(newScream)
         .then((doc) =>{
             res.json({message: `document ${doc.id} created`})
@@ -83,19 +115,19 @@ app.post('/signup', (req,res) => {
     let errors = {};
 
     if(isEmpty(newUser.email)){
-        errors.email = 'must not be empty'
+        errors.email = 'must not be empty';
     }else if(!isEmail(newUser.email)){
-       errors.email = 'Must be a valid email address' 
+       errors.email = 'Must be a valid email address' ;
     }
 
     if(isEmpty(newUser.password)){
-        errors.password = 'must not be empty'
+        errors.password = 'must not be empty';
     }
     if(newUser.password !== newUser.confirmPassword){
-        errors.confirmPassword = 'passwords do not match'
+        errors.confirmPassword = 'passwords do not match';
     }
-    if(isEmpty(handle)){
-        errors.handle = 'must not be empty'
+    if(isEmpty(newUser.handle)){
+        errors.handle = 'must not be empty';
     }
 
     if(Object.keys(errors).length > 0) return res.status(400).json(errors);
@@ -133,6 +165,36 @@ app.post('/signup', (req,res) => {
     }else
         return res.status(500).json({error: err.code});
     });
+})
+
+app.post(`/login`, (req,res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    }
+
+    let errors = {};
+
+    if(isEmpty(user.email))errors.email = 'must not be empty';
+    if(isEmpty(user.password))errors.password = 'must not be empty';
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.user.getIdToken();
+        })
+        .then(token => {
+            return res.json({token});
+        })
+        .catch(err => {
+           console.error(err);
+           if(err.code == 'auth/wrong-password'){
+               return res.status(403).json({general: "Wrong credentials please try again"})
+           }
+            return res.status(500).json({error: err.code})
+        })
+ 
 })
 
 exports.api = functions.https.onRequest(app);
